b448ce92325eb7f55b99beaa01a90802ff636620
chengchengnihao <2164015121@qq.com>
2026-04-20 21:28:16 +0800

    4

---
+++ Source/boy1/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright yuanye 
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/boy1/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright yuanye 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOY1_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
