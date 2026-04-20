e34e2f6db07d83a5bc0ceff93a43132679e9682a
chengchengnihao <2164015121@qq.com>
2026-04-20 12:58:06 +0800

    create aura and enemy blueprint

--- Source/boy1/Private/Character/AuraCharacterBase.cpp
+++ Source/boy1/Private/Character/AuraCharacterBase.cpp
@@ -7,7 +7,15 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
  
 	PrimaryActorTick.bCanEverTick = false;
-    Weawpon=CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+    Weapon=CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	//CreateDefaultSubobject需要FName,FName不需要TEXT宏，FString需要TEXT宏，FString是给人看，FName是给引擎看，所以只用存编号
+	//起名、查找、插槽、组件名 → FName
+	//显示、打印、拼接、内容 → FString
+	//左边的weapon是在c++中被调用
+	//右边的weapon在蓝图中被创建
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 void AAuraCharacterBase::BeginPlay()
