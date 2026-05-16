d07750e47cfcc5028d2aa726e4ddd6a15ab94d91
chengchengnihao <2164015121@qq.com>
2026-05-16 19:05:09 +0800

    4.1,4.2

--- Source/boy1/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/boy1/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,41 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include"AbilitySystem/AuraAbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Health,COND_None,REPNOTIFY_Always); //do_rep_notify
+	//这在注册属性并进行复制
+	//COND_None：无条件进行复制
+	//REPNOTIFY_Always：只要在服务器上设置值，就要进行复制
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Mana,COND_None,REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxMana,COND_None,REPNOTIFY_Always);
+	
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldHealth);
+	//作用是告诉能力系统这个属性的变化
+	//记住旧值，可以在之后回滚的时候用到，还没有定义回滚函数
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana)const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldMaxMana);
+}
--- Source/boy1/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/boy1/Public/AbilitySystem/AuraAttributeSet.h
@@ -6,6 +6,7 @@
 #include "AttributeSet.h"
 #include "AuraAttributeSet.generated.h"
 
+
 /**
  * 
  */
@@ -13,5 +14,33 @@ UCLASS()
 class BOY1_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	//不需要写UFUNCTION()宏，因为它不需要在蓝图里调用，实现，也不需要引擎反射系统识别
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>&OutLifetimeProps)const override;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Health,Category="Vital Attributes")  
+	//当服务器把 Health 同步到客户端时，自动调用客户端的 OnRep_Health() 函数。
+	//大多数属性都需要这样做
+	FGameplayAttributeData Health;   //Health是属性值不是指针，它存储于FGameplayAttributeData结构体中
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxHealth,Category="Vital Attributes")
+	FGameplayAttributeData MaxHealth; 
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Mana,Category="Vital Attributes")
+	FGameplayAttributeData Mana; 
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxMana,Category="Vital Attributes")
+	FGameplayAttributeData MaxMana; 
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth)const;//末尾的const作用是只读取数据，不修改任何成员变量
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)const;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana)const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)const;
+	
 	
 };
